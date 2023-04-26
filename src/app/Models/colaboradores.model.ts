777c74cd001ff81f0b24d3fcee719db8f69b3302
dflores <davidelias-01@hotmail.com>
2023-04-26 16:54:59 -0600

    me centro en diseno

--- src/app/Models/colaboradores.model.ts
+++ src/app/Models/colaboradores.model.ts
@@ -1,12 +1,10 @@
 
 
 export interface Colaborador {
-  id: number,
   primerNombre: string,
   primerApellido: string,
   dni: string,
   fechaNacimiento: Date
   direccion: string,
   telefono: string,
-  activo: boolean,
 }
