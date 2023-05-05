4eaca4c4d8f95cdc1a5b75aedfab3c0e00a3cd9e
David Flores <130876677+DavidElias-maker@users.noreply.github.com>
2023-05-05 14:51:41 -0600

    se hizo el calculo

--- src/app/components/Viajes/viajes-list/viajes-list.component.ts
+++ src/app/components/Viajes/viajes-list/viajes-list.component.ts
@@ -1,10 +1,12 @@
 
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Sucursal_Colaborador } from 'src/app/Models/sucursal_colaboradores.model';
 import { Sucursal_ColaboradorService } from 'src/app/services/sucursal_colaboradores.service';
 import { data } from 'jquery';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { TransportistasService } from 'src/app/services/transportistas.service';
+import { Transportista } from '../../../Models/transportistas.model';
 
 
 
@@ -21,15 +23,19 @@ export class ViajesListComponent implements OnInit {
 
   public sucursales_colaboradores: Sucursal_Colaborador[] = [];
 
-  selectedValues: string[] = [];
+  public transportistas: Transportista[] = [];
+  columnSums: number[] = [];
+  selectedValues1: string[] = [];
+  selectedValues2: string[] = [];
   data:undefined|Sucursal_Colaborador[];
+  data2:undefined|Transportista[];
   ColObj : Sucursal_Colaborador = new Sucursal_Colaborador();
 
 
 
   isCheckAll: boolean = false;
 
-  constructor(private fb: FormBuilder, private Sucursal_ColaboradorServicio: Sucursal_ColaboradorService, private FormBuilder: FormBuilder) {
+  constructor(private fb: FormBuilder, private Sucursal_ColaboradorServicio: Sucursal_ColaboradorService, private FormBuilder: FormBuilder,private TransportistasServicio:TransportistasService) {
 
 
   }
@@ -37,12 +43,16 @@ export class ViajesListComponent implements OnInit {
 
 
 this.getNombreCompleto();
+this.ObtenerAllTransportistas();
 
 this.employeeForm = this.FormBuilder.group({
   tableRows: this.fb.array([],[Validators.required]),
   nombreCompleto: ['',[Validators.required,Validators.minLength(10)]],
   nombre:['',[Validators.required]],
-  distanciaKm: ['',[Validators.required]]
+  distanciaKm: ['',[Validators.required]],
+  primerNombre: ['',[Validators.required]],
+  tarifa: ['',[Validators.required]],
+  multipliedSums: [null]
 });
 
 
@@ -55,7 +65,9 @@ this.getNombreCompleto();
       nombreCompleto: ['',[Validators.required,Validators.minLength(3)]],
       nombre:[''],
       distanciaKm: [''],
-      ischecked: [false]
+      primerNombre:[''],
+      tarifa: ['',[Validators.required]],
+      multipliedSums: [null]
     });
   }
 
@@ -79,7 +91,8 @@ this.getNombreCompleto();
 
   onSaveForm() {
     const formValue = this.employeeForm.value;
-console.log(formValue);
+   console.log(formValue);
+   this.calculateColumnSums()
   }
 
 getNombreCompleto(){
@@ -93,6 +106,12 @@ getNombreCompleto(){
 
   })
 }
+ObtenerAllTransportistas(){
+  this.TransportistasServicio.getAllTransportistas()
+      .subscribe(TtransportistasRecibidos => {
+        this.transportistas = TtransportistasRecibidos;
+      });
+}
 
 
 
@@ -100,11 +119,11 @@ getNombreCompleto(){
 
 
 
-onOptionSelected(event: MatAutocompleteSelectedEvent) {
+onOptionSelected1(event: MatAutocompleteSelectedEvent) {
 
   const value = event.option.value;
-  if (!this.selectedValues.includes(value)) {
-    this.selectedValues.push(value);
+  if (!this.selectedValues1.includes(value)) {
+    this.selectedValues1.push(value);
   }
 
     const tableRows = this.employeeForm.get('tableRows') as FormArray;
@@ -120,17 +139,56 @@ onOptionSelected(event: MatAutocompleteSelectedEvent) {
 
 
     const addedIndex = tableRows.length - 1;
+
     const addedRow = tableRows.at(addedIndex) as FormGroup;
     addedRow.controls['nombre'].setValue(selectedSucursalColaborador?.nombre);
     addedRow.controls['distanciaKm'].setValue(selectedSucursalColaborador?.distanciaKm);
 
   };
 
+  onOptionSelected2(event: MatAutocompleteSelectedEvent) {
 
 
+    const selectedPrimerNombre = event.option.value;
+    const selectedTransportista = this.transportistas.find(item => item.primerNombre === selectedPrimerNombre);
+    this.employeeForm.controls['tarifa'].setValue(selectedTransportista?.tarifa);
 
 
+  }
+
+  calculateColumnSums() {
+    const numColumns = 3;
+    this.columnSums = Array(numColumns).fill(0);
+
+    const tableRows = this.employeeForm.get('tableRows') as FormArray;
+    tableRows.controls.forEach((rowGroup: AbstractControl) => {
+      const row = rowGroup.value;
+      for (let i = 0; i < numColumns; i++) {
+        const columnName = Object.keys(row)[i];
+        const cellValue = row[columnName];
+
+
+        if (!isNaN(cellValue)) {
+          this.columnSums[i] += +cellValue;
+        }
+      }
+    });
+
+    const selectedTransportista = this.employeeForm.get('primerNombre')?.value;
+  const transportista = this.transportistas.find(item => item.primerNombre === selectedTransportista);
+
+
+  let result = 0;
+  if (transportista) {
+    result = this.columnSums.reduce((acc, sum) => acc + sum) * transportista.tarifa;
+  }
+
+  this.employeeForm.get('multipliedSums')?.setValue(result);
+
+  }
 }
+
+
 
 
 
