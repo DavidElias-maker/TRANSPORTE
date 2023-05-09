9136bf16ddc4c8f02de13ad7b8906e088c354356
David Flores <130876677+DavidElias-maker@users.noreply.github.com>
2023-05-09 14:35:42 -0600

    Update dni.pipes.ts
    cmabio

--- src/app/Pipes/dni.pipes.ts
+++ src/app/Pipes/dni.pipes.ts
@@ -6,15 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class dnipipe implements PipeTransform {
 
   transform(value: string): string {
-    // Eliminamos cualquier caracter que no sea un número
+
     const dni = value.replace(/\D/g, '');
 
-    // Comprobamos que el número de DNI tenga una longitud válida
+
     if (dni.length !== 13) {
       return value;
     }
 
-    // Obtenemos el número de DNI con el formato ####-####-#####
+
     const primerosCuatro = dni.substr(0, 4);
     const segundosCuatro = dni.substr(4, 4);
     const ultimosCinco = dni.substr(8, 5);
